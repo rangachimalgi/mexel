3ce21147ec18d410d071c686deb103151ab497bc
rangachimalgi <ranganath.chimalgi@patientry.ai>
2025-04-24 04:07:33 +0530

    projects unnecessary import deleted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Timeline from "../components/Timeline";
-import Projects from "../components/Projects";
 import Contact from "../components/Contact";
 import Clients from "../components/Clients";
 
